1f880aba53279eef9d9196df9632e248a3598fd7
Jin Hong Pang <jpan0069@student.monash.edu>
2026-09-07 17:16:18 +1000

    foass

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ type Params = {
 }
 
 function App() {
-  const [draft, setDraft] = useState<Params>({ name: 'HONG', from: '' })
+  const [draft, setDraft] = useState<Params>({ name: 'HONG', from: 'David Martinez' })
   const [submitted, setSubmitted] = useState<Params | null>(null)
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
